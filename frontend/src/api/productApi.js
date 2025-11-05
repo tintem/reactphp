c169edb15ec080bccc01039a1d7aa87fb286dece
tintem <hung.seu@gmail.com>
2025-11-05 10:57:49 +0700

    Sua api url

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -4,7 +4,7 @@ const API_URL = "https://67b7f5c32bddacfb27107b27.mockapi.io/product";
 const API_URL2='http://localhost:8000/api/product.php';
 export const getAllProducts = async () => {
   try {
-    const res = await axios.get(API_URL2);
+    const res = await axios.get(API_URL);
     return res.data.data;
   } catch (error) {
     console.error("Error fetching products:", error);
