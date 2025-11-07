3da53c94a2f75e91aae25ebf8a2b04d61cc9723f
tintem <hung.seu@gmail.com>
2025-11-07 13:44:43 +0700

    ket hop...

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const API_URL =  "https://67b7f5c32bddacfb27107b27.mockapi.io/product";
  
- cont API_URL ='https://be.manguonmo.top/api/product.php';
+ const API_URL ='https://be.manguonmo.top/api/product.php';
 export const getAllProducts = async () => {
   try {
     const res = await axios.get(API_URL);
